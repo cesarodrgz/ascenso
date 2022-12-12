0f66c28542228bdbbd880c5ae19fb1cbde2ddc04
cesarodrgz <rodriguez1995cesar@gmail.com>
2022-12-11 21:18:16 -0600

    Busqueda SQL del nombre a mostrar

--- modulosUsuarios/navegantes/navegantes.php
+++ modulosUsuarios/navegantes/navegantes.php
@@ -1,7 +1,9 @@
 <?php 
-include "../../templates/conexion.php";
-session_start();
-$user = $_SESSION['user'];
+include "../../templates/conexion.php"; //Incluimos la clase conexión para realizar busquedas SQL
+session_start(); //Iniciamos la variable de sesión
+$user = $_SESSION['user']; //Obtenemos de Inciar sesion el user que esta entrando
+
+$consulta = mysqli_query($enlace, "SELECT nombre from usuarios where user = '$user'");//Realizamos una búsqueda SQL en la tabla usuarios donde el usuario sea igual a lo que traiga la variable de incio de sesión
 ?>
 <!doctype html>
 <html lang="en">
@@ -30,10 +32,17 @@ $user = $_SESSION['user'];
 
     <div class="row">
       <div class="col-xs-12 col-md-6 col-xl-12">
-        <div class="alert alert-primary" role="alert">
-          <h2 class="text-center">
-            Bienvenido <?php echo $user //Aquí debe aparecer el nombre (realizar una búsqueda SQL) ?> 
-          </h2>
+        <div class="alert alert-success" role="alert">
+          <h4 class="text-center">
+            Bienvenido: 
+            <?php 
+            //Mostramos los datos que coincidan con la consulta SQL realizada
+              while ($datos = mysqli_fetch_array($consulta)) {
+                //Mostramos el nombre el usuario que obtenemos de la consulta realizada
+               echo $datos["nombre"]; 
+              }
+            ?>
+          </h4>
         </div>
       </div>
 

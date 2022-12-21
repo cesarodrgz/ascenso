119d7a5c13ae606dbd8436bab391d0a5137204e3
Josue2819 <105530372+Josue2819@users.noreply.github.com>
2022-12-21 15:00:15 -0600

    moduloadmin-navegantes

---
+++ js/dataTable.js
@@ -0,0 +1,12 @@
+var table = $('#usuarios').DataTable({
+    language: {
+        "search": "Buscar:",
+        "zeroRecords": "Sin resultados encontrados",
+        "paginate": {
+            "first": "Primero",
+            "last": "Ultimo",
+            "next": "Siguiente",
+            "previous": "Anterior"
+        }
+    },
+});
---
+++ moduloAdmin/navengantes/opciones.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Opciones Administrador</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <br>
+    <div class="container">
+        <div class="col">
+            <div class="col-xs-12 col-md-6 col-xl-12">
+                <h1 class="text-center">Opciones</h1>
+                <br>
+                <br>
+                <center>
+                    <img src="../../img/jefe.png" alt="admin" style="width: 150px;">
+                </center>
+                <a href="administrarusuario.php" class="btn btn-primary btn-block">Administrar Usuario</a>
+            </div>
+
+            <div class="col-xs-12 col-md-6 col-xl-12">
+                <br>
+                <br>
+                <center>
+                    <img src="../../img/agregar.png" alt="AGREGAR-PREMIOS" style="width: 125px;">
+                </center>
+                <br>
+                <a href="" class="btn btn-primary btn-block">Agregar Premios</a>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>

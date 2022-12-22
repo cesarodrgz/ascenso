df9b5fae75ba4f6aa524a7a3b7426f69966e950d
cesarodrgz <rodriguez1995cesar@gmail.com>
2022-12-21 21:53:06 -0600

    IDIOMA-DATATABLE

--- js/dataTable.js
+++ js/dataTable.js
@@ -2,6 +2,9 @@ var table = $('#usuarios').DataTable({
     language: {
         "search": "Buscar:",
         "zeroRecords": "Sin resultados encontrados",
+        "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+        "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+        "lengthMenu": "Mostrar _MENU_ Entradas",
         "paginate": {
             "first": "Primero",
             "last": "Ultimo",

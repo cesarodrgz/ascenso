f4a0afd358c15de5c24f710d364ae9a9536627ce
cesarodrgz <rodriguez1995cesar@gmail.com>
2023-08-08 23:35:30 -0600

    Mostrar 3 años - Exploradores

--- modulosUsuarios/exploradores/VerPremios.php
+++ modulosUsuarios/exploradores/VerPremios.php
@@ -3,8 +3,9 @@ include "../../templates/conexion.php"; //Incluimos la clase conexión para real
 session_start(); //Iniciamos la variable de sesión
 $user = $_SESSION['user']; //Obtenemos de Inciar sesion el user que esta entrando
 
-$consulta = mysqli_query($enlace, "SELECT * from explobronce WHERE user = '$user'"); //Realizamos una búsqueda SQL en la tabla usuarios donde el usuario sea igual a lo que traiga la variable de incio de sesión
-$consulta2 = mysqli_query($enlace, "SELECT * from exploplata WHERE user = '$user'"); //Realizamos una búsqueda SQL en la tabla usuarios donde el usuario sea igual a lo que traiga la variable de incio de sesión
+$consulta = mysqli_query($enlace, "SELECT * from explobronce WHERE user = '$user'"); //Realizamos una búsqueda SQL en la tabla de los premios del año 1
+$consulta2 = mysqli_query($enlace, "SELECT * from exploplata WHERE user = '$user'"); //Realizamos una búsqueda SQL en la tabla de los premios del año 2
+$consulta3 = mysqli_query($enlace, "SELECT * from explooro WHERE user = '$user'"); //Realizamos una búsqueda SQL en la tabla de los premios del año 3
 ?>
 <!doctype html>
 <html lang="en">
@@ -86,6 +87,40 @@ $consulta2 = mysqli_query($enlace, "SELECT * from exploplata WHERE user = '$user
                                     <td><?php echo $datos2["p4"]; ?></td>
                                     <td><?php echo $datos2["p5"]; ?></td>
                                     <td><?php echo $datos2["p6"]; ?></td>
+                                </tr>
+                            <?php
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-12 col-md-12 col-xl-12">
+                <h3 class="text-center text-success">Premios obtenidos en el Año 3</h3>
+                <div class="table-responsive-md">
+                    <table class="table">
+                        <tr>
+                            <thead>
+                                <td><b>Premio 1</b></td>
+                                <td><b>Premio 2</b></td>
+                                <td><b>Premio 3</b></td>
+                                <td><b>Premio 4</b></td>
+                                <td><b>Premio 5</b></td>
+                            </thead>
+                        </tr>
+                        <tbody>
+                            <?php
+                            //Mostramos los datos que coincidan con la consulta SQL realizada
+                            while ($datos2 = mysqli_fetch_array($consulta3)) { ?>
+                                <tr>
+                                    <td><?php echo $datos2["p1"]; ?></td>
+                                    <td><?php echo $datos2["p2"]; ?></td>
+                                    <td><?php echo $datos2["p3"]; ?></td>
+                                    <td><?php echo $datos2["p4"]; ?></td>
+                                    <td><?php echo $datos2["p5"]; ?></td>
                                 </tr>
                             <?php
                             }

6d861175db8a1bdd6975302dab9c0a019336b2d8
cesarodrgz <rodriguez1995cesar@gmail.com>
2023-08-19 21:42:13 -0600

    cambios login

--- templates/iniciarS.php
+++ templates/iniciarS.php
@@ -6,6 +6,10 @@ include 'conexion.php';
 $correo = $_POST['user']; //input del correo que llamaremos usuario
 $contra = $_POST['pass']; //input de la contraseña
 
+/////////////////VALIDACIÓN DE LOS LOGIN/////////////////
+
+/////////VALIDACIÓN NAVEGANTES/////////
+
 //Consulta SQL que usaremos para buscar en la base de datos el usuario
 $consulta = "SELECT user, pass, departamento FROM usuarios WHERE user = '" . $correo . "' and pass = '" . $contra . "' and departamento = 'Navegantes'";
 $resultado = mysqli_query($enlace, $consulta);
@@ -13,63 +17,59 @@ $resultado = mysqli_query($enlace, $consulta);
 //Obtenemos el resultados mediante las filas
 $filas = mysqli_num_rows($resultado);
 
-/////////////////VALIDACIÓN DE LOS LOGIN/////////////////
-
-/////////VALIDACIÓN NAVEGANTES/////////
-
-//Si existe un número mayor que cero, enviamos al usuario a la web de opciones
+//Si el usuario existe lo manda al modulo correspondiente.
 if ($filas > 0) {
     header("location:../modulosUsuarios/navegantes/navegantes.php");
-    //Si el usuario no esta lo enviamos al index nuevamente, es decir el usuario no existe.
-} elseif ($filas < 0) {
-    header("location:../login.php");
 } else {
-    /*Comprobamos que el correo y contraseña están o no 
-    en la tabla de usuarios*/
+    /////////VALIDACIÓN ADMIN NAVEGANTES/////////
+
+    //Consulta SQL que usaremos para buscar en la base de datos el usuario
     $consulta = "SELECT user, pass, departamento FROM usuarios WHERE user = '" . $correo . "' and pass = '" . $contra . "' and departamento = 'administradorNav'";
     $resultado = mysqli_query($enlace, $consulta);
 
-    //Si hay un resultado lo mandamos al index administrativo sino al index general
+    //Obtenemos el resultados mediante las filas
     $filas = mysqli_num_rows($resultado);
+
+    //Si el usuario existe lo manda al modulo correspondiente.
     if ($filas > 0) {
         header("location:../moduloAdmin/navegantes/opciones.php");
-    } elseif ($filas < 0) {
-        header("location:../index.php");
     } else {
 
-        /////////////////VALIDACIÓN EXPLORADORES/////////////////
+        /////////VALIDACIÓN EXPLORADORES/////////
 
-        /*Comprobamos que el correo y contraseña están o no 
-        en la tabla de usuarios*/
-        $consulta = "SELECT user, pass, departamento FROM usuarios WHERE user = '" . $correo . "' and pass = '" . $contra . "' and departamento = 'administradorEx'";
+        //Consulta SQL que usaremos para buscar en la base de datos el usuario
+        $consulta = "SELECT user, pass, departamento FROM usuarios WHERE user = '" . $correo . "' and pass = '" . $contra . "' and departamento = 'Exploradores'";
         $resultado = mysqli_query($enlace, $consulta);
 
-        //Si hay un resultado lo mandamos al index administrativo sino al index general
+        //Obtenemos el resultados mediante las filas
         $filas = mysqli_num_rows($resultado);
-        if ($filas > 0) {
-            header("location:../moduloAdmin/exploradores/opciones.php");
-        } elseif ($filas < 0) {
-            header("location:../index.php");
-        } else {
 
-            ////////////LOGIN ADMIN GENERAL///////////
-            /*Comprobamos que el correo y contraseña están o no 
-            en la tabla de usuarios*/
-            $consulta = "SELECT user, pass, departamento FROM usuarios WHERE user = '" . $correo . "' and pass = '" . $contra . "' and departamento = 'AdminGen'";
+        //Si el usuario existe lo manda al modulo correspondiente.
+        if ($filas > 0) {
+            header("location:../modulosUsuarios/exploradores/exploradores.php");
+        } else {
+            /////////VALIDACIÓN ADMIN NAVEGANTES/////////
+
+            //Consulta SQL que usaremos para buscar en la base de datos el usuario
+            $consulta = "SELECT user, pass, departamento FROM usuarios WHERE user = '" . $correo . "' and pass = '" . $contra . "' and departamento = 'administradorEx'";
             $resultado = mysqli_query($enlace, $consulta);
-            //Si hay un resultado lo mandamos al index administrativo sino al index general
+
+            //Obtenemos el resultados mediante las filas
             $filas = mysqli_num_rows($resultado);
 
+            //Si el usuario existe lo manda al modulo correspondiente.
             if ($filas > 0) {
-                header("location:../adming/index.php");
-            } elseif ($filas < 0) {
-                header("location:../index.php");
+                header("location:../moduloAdmin/exploradores/opciones.php");
             } else {
-                
+                header("location:baduser.php");
             }
         }
     }
 }
+
+
+
+
 
 //Iniciamos una sesión (esta nos sirve para mantener uno o varios datos a la vez del usuario ingresado)
 session_start();

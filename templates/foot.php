57b91218ddf7a2d67685ca389df5fcbaa237ad66
cesarodrgz <rodriguez1995cesar@gmail.com>
2023-09-13 16:52:54 -0600

    cambios footer

--- templates/foot.php
+++ templates/foot.php
@@ -4,28 +4,24 @@
 <!------ Include the above in your HEAD tag ---------->
 
 <!-- Footer -->
-	<section id="footer">
-		<div class="container">
-			<div class="row">
-				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
-					<ul class="list-unstyled list-inline social text-center">
-						<li class="list-inline-item"><a href="https://www.facebook.com/ExploradoresDelReyLeonDeJuda10Sm"><i class="fa fa-facebook"></i></a></li>
-						<li class="list-inline-item"><a href="#"><i class="fa fa-twitter"></i></a></li>
-						<li class="list-inline-item"><a href="#"><i class="fa fa-instagram"></i></a></li>
-						<li class="list-inline-item"><a href="#"><i class="fa fa-google-plus"></i></a></li>
-						<li class="list-inline-item"><a href="#" target="_blank"><i class="fa fa-envelope"></i></a></li>
-					</ul>
-				</div>
-				<hr>
-			</div>	
-			<div class="row">
-				<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
-					<p>Sistema de control de ascenso León de Judá 10 San Miguel, El Salvador.</p>
-					<p class="h6">© All right Reversed.</p>
-                    <p class="h6">Versión 1.0</p>
-				</div>
-				<hr>
-			</div>	
+<section id="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+				<ul class="list-unstyled list-inline social text-center">
+					<li class="list-inline-item"><a href="https://www.facebook.com/ExploradoresDelReyLeonDeJuda10Sm"><i class="fa fa-facebook"></i></a></li>
+				</ul>
+			</div>
+			<hr>
 		</div>
-	</section>
-	<!-- ./Footer -->
+		<div class="row">
+			<div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+				<p>Sistema de control de ascenso León de Judá 10 San Miguel, El Salvador.</p>
+				<p class="h6">© All right Reversed.</p>
+				<p class="h6">Versión 1.0</p>
+			</div>
+			<hr>
+		</div>
+	</div>
+</section>
+<!-- ./Footer -->
